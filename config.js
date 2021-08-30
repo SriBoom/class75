004a0c42655bae47a889c331c0c2029c05fcf0d2
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-08-29 19:44:53 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "wily-app-7a93e",
   storageBucket: "wily-app-7a93e.appspot.com",
   messagingSenderId: "836025592643",
-  appId: "1:836025592643:web:8fdfa58815a5f4552a9df6"
+  appId: "1:836025592643:web:ac1326855fb308892a9df6"
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
